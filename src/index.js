d223b603f1b321bf51737b3280fbb951a1e77d07
DavidRMorphew <morphewd@umich.edu>
2021-04-18 10:59:41 -0400

    code for testing out original-pick highlighting added to css file and index.js file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ const gamesURL = baseURL + "games"
 const usersURL = baseURL + "users"
 const currentGameContainer = document.getElementById("current-game-container")
 
+// The following is for testing
+const testGame = new Game(1)
+testGame.initializeRandomGame()
+testGame.renderGameDisplay()
+
+door1 = document.getElementById("door1")
