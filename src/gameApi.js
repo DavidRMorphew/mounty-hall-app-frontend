dfd6a82311cf08f823218a685486c3c95310dc7a
DavidRMorphew <morphewd@umich.edu>
2021-04-19 17:20:52 -0400

    user_id added to gameResults object in GameApi.getGames()

--- src/gameApi.js
+++ src/gameApi.js
@@ -7,11 +7,10 @@ class GameApi {
         .then(gameData => {
             console.log(gameData["data"]);
             gameData['data'].forEach(gameObj => {
-                const gameResults = new Game({id: gameObj.id, ...gameObj.attributes})
+                const gameResults = new Game({id: gameObj.id, user_id: gameObj.relationships.user.data.id, ...gameObj.attributes})
                 console.log("game object: "+gameResults)
                 debugger
             })
-            // debugger
         })
     }   
 }
