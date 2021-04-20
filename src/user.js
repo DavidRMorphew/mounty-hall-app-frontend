2d17409150a541e35cc50e99840d9c5d4b40794a
DavidRMorphew <morphewd@umich.edu>
2021-04-20 13:05:05 -0400

    title for userSelectionContainer added to addNewUserFormToDom method

--- src/user.js
+++ src/user.js
@@ -5,6 +5,9 @@ class User {
     };
 
     static addNewUserFormToDOM(){
+        const selectUserTitle = document.createElement('h2')
+        selectUserTitle.innerText = "Type a username below to find a user on file or create a new user. Alternatively, use the dropdown menu to the right to select a user."
+        userSelectionContainer.appendChild(selectUserTitle)
         const newUserForm = document.createElement('form')
         newUserForm.innerHTML = 
         `<label for="username">New User Name</label>
