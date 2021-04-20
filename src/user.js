bae4c31f49670e1fa02f1ed4b2dac4b14f23907e
DavidRMorphew <morphewd@umich.edu>
2021-04-19 23:17:03 -0400

    addNewUserFormToDOM built with event listener preventing default

--- src/user.js
+++ src/user.js
@@ -7,10 +7,15 @@ class User {
     static addNewUserFormToDOM(){
         const newUserForm = document.createElement('form')
         newUserForm.innerHTML = 
-        `<input type="text" name="username">
+        `<label for="username">New User Name</label>
+        <input type="text" id="username" name="username">
         <input type="submit" value="submit">`
         newUserForm.id = "new-user-form"
         userSelectionContainer.appendChild(newUserForm)
+        newUserForm.addEventListener('submit', event => {
+            event.preventDefault()
+            console.log(event.target)
+        })
     };
 
 }
